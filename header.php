e826c15b84c672755ae2282fc5d1662739e0daa9
ziaddiab74 <62486507+ziaddiab74@users.noreply.github.com>
2020-03-22 12:36:31 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,43 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link type="text/css" rel="stylesheet" href="../assets/css/lib/bootstrap.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/924a7f6fcf.js" crossorigin="anonymous"></script>
+    <link type="text/css" rel="stylesheet" href="../assets/css/footer.css">
+</head>
+
+<body>
+    <div id="contain" class="image-container">
+        <div class="container">
+            <br>
+            <div class="col-lg-12">
+              
+                    
+                        
+                </div>
+                
+                    <h2 style="text-align:center; margin-bottom: 0px">Cairo Hospital</h2>
+                    <br>
+                    
+                     
+                </div>
+           
+
+        <div class="container">
+            <hr>
+            <div class="col-lg-12">
+                <div style="text-align: center">
+                    <p style="text-align: center; display: inline-block"></p>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
