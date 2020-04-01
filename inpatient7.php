a896e40149911340f30f5e08083b509ac314b1f4
ziaddiab74 <62486507+ziaddiab74@users.noreply.github.com>
2020-04-01 16:28:51 +0200

    Update inpatient7.php

--- inpatient7.php
+++ inpatient7.php
@@ -20,50 +20,122 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 <!-- Overlay effect when opening sidebar on small screens -->
 <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
 
-
-  <!-- Header -->
-  <header id="portfolio">
-    
-    <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
-    <div class="w3-container">
-    <h1><b>INPATIENT CLINICS</b></h1>
-    <div class="w3-section w3-bottombar w3-padding-16">
-    <a href="inpatient.php"><button class="w3-button w3-white"><i class="fa fa-map-pin w3-margin-right"></i>Surgery</button>
-    <a href="inpatient2.php"><button class="w3-button w3-white"><i class="fa fa-map-pin w3-margin-right"></i>Skin</button>
-    <a href="inpatient3.php"><button class="w3-button w3-white"><i class="fa fa-map-pin w3-margin-right"></i>Cardiology</button>
-    <a href="inpatient4.php"><button class="w3-button w3-white"><i class="fa fa-map-pin w3-margin-right"></i>Ear,Nose and Throat</button>
-    <a href="inpatient5.php"><button class="w3-button w3-white"><i class="fa fa-map-pin w3-margin-right"></i>Dental</button>
-    <a href="inpatient6.php"><button class="w3-button w3-white"><i class="fa fa-map-pin w3-margin-right"></i>Laboratory</button>
-    <a href="inpatient7.php"><button class="w3-button w3-white"><i class="fa fa-map-pin w3-margin-right"></i>Vascular Surgery</button>
-    </div>
-    
-  </header>
+<div class="w3-container" id="services" style="margin-top:10px">
+     <div class="dropdown">
+  <button type="button" class="w3-bar-item w3-button w3-text-grey w3-xxlarge w3-hover-white dropdown-toggle" data-toggle="dropdown">
+  Inpatient Clinics --> Vascular Surgery
+  </button>
+  <div class="dropdown-menu" style="margin-left:280px">
+    <a href="inpatient.php" onclick="w3_close()" class="dropdown-item" href="#">Skin</a>
+    <a href="inpatient2.php" onclick="w3_close()" class="dropdown-item" href="#">Surgery</a>
+    <a href="inpatient3.php" onclick="w3_close()" class="dropdown-item" href="#">Cardiology</a>
+    <a href="inpatient4.php" onclick="w3_close()" class="dropdown-item" href="#">Ear,Nose and Throat</a>
+    <a href="inpatient5.php" onclick="w3_close()" class="dropdown-item" href="#">Dental</a>
+    <a href="inpatient6.php" onclick="w3_close()" class="dropdown-item" href="#">Laboratory</a>
+    <a href="inpatient7.php" onclick="w3_close()" class="dropdown-item" href="#">Vascular Surgery</a>
+  </div>
+</div>
   
   <!-- First Photo Grid-->
   <div class="w3-row-padding">
     <div class="w3-third w3-container w3-margin-bottom">
       <div class="w3-container w3-white w3-xlarge ">
-        <p><b>DR.AHMAD</b></p></div>
+        <p><b>DR.Khaled</b></p></div>
         <div class="w3-container w3-white ">
-        <p>Department:Surgery</p>
+        <p>Department:Skin</p>
         <p>Designation:Specialist</p>
 <p>Recent Positions:</p>
 <p>Asst. Lecturer of surgery MedicineAsst.
 <p> Lecturer of surgery MedicineSuez Canal univ</p>
-      <button id="submitSignUp" class="btn btn-success btn-lg btn-block">book</button>
-      </div>
+
+  
+  <!-- Button to Open the Modal -->
+  <button type="button" class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">
+    Book
+  </button>
+
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Receipt</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          <p>Dr.Ziad
+          <p>Date: 20/3/2020
+          <p>Waiting Customer: 2
+          <p>Time: 6:00pm
+          <P>Amount: 200LE
+
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      
     </div>
+  </div>
+  
+</div>
+
+      </div>
+      
+    </div>
+    <!--..............................................................................................................-->
     <div class="w3-third w3-container w3-margin-bottom">
     <div class="w3-container w3-white w3-xlarge ">
         <p><b>DR.YOUSSEF</b></p></div>
         <div class="w3-container w3-white">
-        <p>Department:Anaesthesia
+        <p>Department:Skin
         <p>Designation:Specialist
         <p>Recent Positions:
         <p>Anaesthesia Specialist SGH-Jeddah
 
         <p>Anaesthesia Resident, El Hela</p>
-        <button id="book" class="btn btn-success btn-lg btn-block">book</button>
+         <!-- Button to Open the Modal -->
+  <button type="button" class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">
+    Book
+  </button>
+
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Receipt</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          <p>Dr.Ziad
+          <p>Date: 20/3/2020
+          <p>Waiting Customer: 2
+          <p>Time: 6:00pm
+          <P>Amount: 200LE
+
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      
+    </div>
+  </div>
+  
+</div>
 
       </div>
     </div>
@@ -71,16 +143,53 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
     <div class="w3-container w3-white w3-xlarge ">
         <p><b>DR.ZIAD</b></p></div>
         <div class="w3-container w3-white">
-        <p>Department:Cardiology
+        <p>Department:Skin
 <p>Designation:Consultant</p>
 <p>Recent Positions:
         <p>Anaesthesia Specialist SGH-Jeddah
 
         <p>Anaesthesia Resident, El Hela</p>
-        <button id="book" class="btn btn-success btn-lg btn-block">book</button>
+         <!-- Button to Open the Modal -->
+  <button type="button" class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">
+    Book
+  </button>
+
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Receipt</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          <p>Dr.Ziad
+          <p>Date: 20/3/2020
+          <p>Waiting Customer: 2
+          <p>Time: 6:00pm
+          <P>Amount: 200LE
+
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      
+    </div>
+  </div>
+  
+</div>
 
       </div>
+      
     </div>
+    
   </div>
   
   <!-- Second Photo Grid-->
@@ -89,12 +198,47 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
     <div class="w3-container w3-white w3-xlarge ">
         <p><b>DR.MOHAMED</b></p></div>
         <div class="w3-container w3-white">
-        <p>Department:Internal Medicine/GIT & Endoscopy
+        <p>Department:Skin
 <p>Designation:Consultan</p>
 <p>Recent Positions:
         <p>Anaesthesia Specialist SGH-Jeddah
         <p>Anaesthesia Resident, El Hela</p>
-        <button id="book" class="btn btn-success btn-lg btn-block">book</button>
+         <!-- Button to Open the Modal -->
+  <button type="button" class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">
+    Book
+  </button>
+
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Receipt</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          <p>Dr.Ziad
+          <p>Date: 20/3/2020
+          <p>Waiting Customer: 2
+          <p>Time: 6:00pm
+          <P>Amount: 200LE
+
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      
+    </div>
+  </div>
+  
+</div>
       </div>
     </div>
 
@@ -102,33 +246,99 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
     <div class="w3-container w3-white w3-xlarge ">
         <p><b>DR.AHMAD MOHAMED</b></p></div>
         <div class="w3-container w3-white">
-        <p>Department:Neurosurgery
+        <p>Department:Skin
 <P>Designation:Consultant
 <p>Recent Positions:
         <p>Anaesthesia Specialist SGH-Jeddah
         <p>Anaesthesia Resident, El Hela</p>
-        <button id="book" class="btn btn-success btn-lg btn-block">book</button>
+ <!-- Button to Open the Modal -->
+ <button type="button" class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">
+    Book
+  </button>
 
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Receipt</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          <p>Dr.Ziad
+          <p>Date: 20/3/2020
+          <p>Waiting Customer: 2
+          <p>Time: 6:00pm
+          <P>Amount: 200LE
+
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      
+    </div>
+  </div>
+  
+</div>
       </div>
     </div>
     <div class="w3-third w3-container">
     <div class="w3-container w3-white w3-xlarge ">
         <p><b>DR.KHALED</b></p></div>
         <div class="w3-container w3-white">
-        <p>Department:Emergency
+        <p>Department:Skin
         <p>Designation:Specialist
         <p>Recent Positions:
 <p>Asst. Lecturer of Emergency MedicineAsst.
 
 <p>Lecturer of Emergency MedicineSuez Canal univ.</p>
-        <button id="book" class="btn btn-success btn-lg btn-block">book</button>
+ <!-- Button to Open the Modal -->
+ <button type="button" class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">
+    Book
+  </button>
 
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Receipt</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          <p>Dr.Ziad
+          <p>Date: 20/3/2020
+          <p>Waiting Customer: 2
+          <p>Time: 6:00pm
+          <P>Amount: 200LE
+
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      
+    </div>
+  </div>
+  
+</div>
       </div>
     </div>
   </div>
-  </div>
-
-
-<?php include('footer.php'); ?>
+  <?php include('footer.php'); ?>
 </body>
+
 </html>
