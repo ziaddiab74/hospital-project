fc52214f2dd2edd61bed15605c5db0d80c5e6e75
ziaddiab74 <62486507+ziaddiab74@users.noreply.github.com>
2020-05-29 04:43:15 +0200

    Add files via upload

---
+++ register.php
@@ -0,0 +1,203 @@
+<?php
+// include Function  file
+include_once('function.php');
+// Object creation
+$userdata=new DB_con();
+if(isset($_POST['submit']))
+{
+// Posted Values
+$fname=$_POST['fullname'];
+$phone=$_POST['phone'];
+$age=$_POST['age'];
+$uname=$_POST['username'];
+$uemail=$_POST['email'];
+$pasword=md5($_POST['password']);
+//Function Calling
+$sql=$userdata->registration($fname,$phone,$age,$uname,$uemail,$pasword);
+if($sql)
+{
+// Message for successfull insertion
+echo "<script>alert('Registration successfull.');</script>";
+echo "<script>window.location.href='signin.php'</script>";
+}
+else
+{
+// Message for unsuccessfull insertion
+echo "<script>alert('Something went wrong. Please try again');</script>";
+echo "<script>window.location.href='signin.php'</script>";
+}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <title>HOSPITAL PROJECT</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+  <nav class="navbar py-4 navbar-expand-lg ftco_navbar navbar-light bg-light flex-row">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-start align-items-center px-3 px-md-0">
+    			<div class="col-lg-2 pr-4 align-items-center">
+		    		<a class="navbar-brand" href="index.html">Cairo.<span>Hospital</span></a>
+	    		</div>
+	    		<div class="col-lg-10 d-none d-md-block">
+		    		<div class="row d-flex">
+			    		<div class="col-md-4 pr-4 d-flex topper align-items-center">
+			    			<div class="icon bg-white mr-2 d-flex justify-content-center align-items-center"><span class="icon-map"></span></div>
+						    <span class="text">Address: 198 West 21th Street, Australia</span>
+					    </div>
+					    <div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon bg-white mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+						    <span class="text">Email: ziaddiab74@l@email.com</span>
+					    </div>
+					    <div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon bg-white mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <span class="text">Phone: 01100036613</span>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+    </nav>
+<head>
+    <meta charset="utf-8">
+   
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="assests/style.css" rel="stylesheet">
+    <script src="assests/jquery-1.11.1.min.js"></script>
+    <script src="assests/bootstrap.min.js"></script>
+ <script>
+function checkusername(va) {
+  $.ajax({
+  type: "POST",
+  url: "check_availability.php",
+  data:'username='+va,
+  success: function(data){
+  $("#usernameavailblty").html(data);
+  }
+  });
+
+}
+</script>
+</head>
+<body>
+<form class="form-horizontal" action='' method="POST">
+  <fieldset>
+    
+
+  <nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
+    <div class="container">
+    <a class="navbar-brand" href="#">Cairo Hospital</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+   
+    </div>
+</nav>
+
+<main class="my-form">
+    <div class="cotainer">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Register</div>
+                        <div class="card-body">
+                            <form name="my-form" onsubmit="return validform()" action="success.php" method="">
+                                <div class="form-group row">
+                                    <label for="full_name" class="col-md-4 col-form-label text-md-right">Full Name</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="full_name" class="form-control" name="fullname"required>
+                                    </div>
+                                </div>
+                         <div class="form-group row">
+                                    <label for="phone_number" class="col-md-4 col-form-label text-md-right">Phone Number</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="phone_number" class="form-control" name="phone" pattern="^[0-9\-\+]{11}$" required>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="nid_number" class="col-md-4 col-form-label text-md-right">age</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="nid_number" class="form-control" name="age" pattern="^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$" required>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="user_name" class="col-md-4 col-form-label text-md-right">User Name</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="user_name" class="form-control" name="username" onblur="checkusername(this.value)" class="input-xlarge" required>
+                                        <span id="usernameavailblty"></span>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="email_address" class="form-control" name="email" pattern="[^@\s]+@[^@\s]+\.[^@\s]+" required>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="email_address" class="col-md-4 col-form-label text-md-right">password</label>
+                                    <div class="col-md-6">
+                                        <input type="password" id="password" class="form-control" name="password" placeholder="" pattern="^\S{6,}$" required>
+                                    </div>
+                                </div>
+                                
+
+                                <div class="control-group">
+      <!-- Button -->
+      <div class="controls">
+        <button class="btn btn-success" type="submit" id="submit" name="submit">Register</button>
+      </div>
+    </div>
+
+ <div class="control-group">
+      <div class="controls">
+       Already registered <a href="signin.php">Signin</a>
+      </div>
+    </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+
+  </fieldset>
+</form>
+<script type="text/javascript">
+</script>
+</body>
+</html>
