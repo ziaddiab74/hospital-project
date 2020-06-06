eb49311e843041dc1ed629ec03f4e2daf5cd8083
ziaddiab74 <62486507+ziaddiab74@users.noreply.github.com>
2020-06-06 19:04:20 +0200

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,162 @@
+<?php
+include("header.php");
+if(isset($_POST[submit]))
+{  
+	$message = "<strong>Dear $_POST[name],</strong><br />
+				<strong>Your Email ID is :</strong> $_POST[email]<br />
+				<strong>Message :-</strong> $_POST[comment]
+				";
+	
+	sendmail("yashikachinz1997@gmail.com","Mail from Appoint My Doctor",$message);
+	
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <title>HOSPITAL PROJECT</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+  <nav class="navbar py-4 navbar-expand-lg ftco_navbar navbar-light bg-light flex-row">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-start align-items-center px-3 px-md-0">
+    			<div class="col-lg-2 pr-4 align-items-center">
+		    		<a class="navbar-brand" href="index.html">Cairo.<span>Hospital</span></a>
+	    		</div>
+	    		<div class="col-lg-10 d-none d-md-block">
+		    		<div class="row d-flex">
+			    		<div class="col-md-4 pr-4 d-flex topper align-items-center">
+			    			<div class="icon bg-white mr-2 d-flex justify-content-center align-items-center"><span class="icon-map"></span></div>
+						    <span class="text">Address: 198 West 21th Street, Australia</span>
+					    </div>
+					    <div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon bg-white mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+						    <span class="text">Email: ziaddiab74@l@email.com</span>
+					    </div>
+					    <div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon bg-white mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <span class="text">Phone: 01100036613</span>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+    </nav>
+<div class="wrapper col2">
+  <div id="breadcrumb">
+    <ul>
+      <li class="first">Contact Us</li>
+    </ul>
+  </div>
+</div>
+<div class="wrapper col4">
+  <div id="container">
+    <h6>Our Address</h6>
+    <p>
+Online Hospital Management System , Bangalore<br />
+
+<strong>tel</strong>:080 65110488<br />
+
+<strong>Email ID</strong>: ohms@gmail.com</p>
+
+        <h6>Contact Us by entering following information</h6>
+            <form action="" method="post">
+          <p>
+            <input type="text" name="name" id="name" value="" size="22" />
+            <label for="name"><small>Name (required)</small></label>
+          </p>
+          <p>
+            <input type="text" name="email" id="email" value="" size="22" />
+            <label for="email"><small>Mail (required)</small></label>
+          </p>
+          <p>
+            <textarea name="comment" id="comment" cols="100%" rows="10"></textarea>
+            <label for="comment" style="display:none;"><small>Comment (required)</small></label>
+          </p>
+          <p>
+            <input name="submit" type="submit" id="submit" value="Submit Form"  />
+            &nbsp;
+            <input name="reset" type="reset" id="reset" tabindex="5" value="Reset Form" />
+          </p>
+        </form>
+
+  </div>
+  
+</div>
+
+    <div class="clear"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+function sendmail($toaddress,$subject,$message)
+{
+	require 'PHPMailer-master/PHPMailerAutoload.php';
+	
+	$mail = new PHPMailer;
+	
+	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+	
+	$mail->isSMTP();                                      // Set mailer to use SMTP
+	$mail->Host = 'mail.dentaldiary.in';  // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true;                               // Enable SMTP authentication
+	$mail->Username = 'sendmail@dentaldiary.in';                 // SMTP username
+	$mail->Password = 'q1w2e3r4/';                           // SMTP password
+	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 587;                                    // TCP port to connect to
+	
+	$mail->From = 'sendmail@dentaldiary.in';
+	$mail->FromName = 'Web Mall';
+	$mail->addAddress($toaddress, 'Joe User');     // Add a recipient
+	$mail->addAddress($toaddress);               // Name is optional
+	$mail->addReplyTo('aravinda@technopulse.in', 'Information');
+	$mail->addCC('cc@example.com');
+	$mail->addBCC('bcc@example.com');
+	
+	$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+	$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+	$mail->isHTML(true);                                  // Set email format to HTML
+	
+	$mail->Subject = $subject;
+	$mail->Body    = $message;
+	$mail->AltBody = $subject;
+	
+	if(!$mail->send()) {
+		echo 'Message could not be sent.';
+		echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+		echo '<center><strong><font color=green>Mail sent.</font></strong></center>';
+	}
+}
+?>
