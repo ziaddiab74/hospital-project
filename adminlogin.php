ea5b3bfb05226fc6e9cd4a0573adb00fa758ec1a
omarbaadry <66132636+omarbaadry@users.noreply.github.com>
2020-06-09 13:58:44 +0200

    Add files via upload

--- adminlogin.php
+++ adminlogin.php
@@ -63,7 +63,7 @@ if(isset($_POST[submit]))
     	<div class="container">
     		<div class="row no-gutters d-flex align-items-start align-items-center px-3 px-md-0">
     			<div class="col-lg-2 pr-4 align-items-center">
-		    		<a class="navbar-brand" href="index.html">Cairo.<span>Hospital</span></a>
+		    		<a class="navbar-brand" >Cairo.<span>Hospital</span></a>
 	    		</div>
 	    		<div class="col-lg-10 d-none d-md-block">
 		    		<div class="row d-flex">
@@ -111,6 +111,13 @@ if(isset($_POST[submit]))
     </table>
     </form>
     <p>&nbsp;</p>
+	<button onclick="goBack()">Go Back</button>
+
+<script>
+function goBack() {
+  window.history.back();
+}
+</script>
   </div>
 </div>
 </div>
